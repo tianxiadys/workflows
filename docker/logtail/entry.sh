c4d9ef20e09defad23887f259d70054807447ec5
gaomingyu <874774259@qq.com>
2025-06-22 00:42:34 +0800

    logtail

--- docker/logtail/entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-/app/bin/ilogtail \
--https_ca_cert "/app/resources/ca-bundle.crt" \
--ilogtail_config "/app/conf/$REGION/ilogtail_config.json" \
--ilogtail_daemon_flag "false" \
--ilogtail_user_defined_id_env_name "$USERID"
